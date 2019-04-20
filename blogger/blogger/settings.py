5e69307d12ebb3fb13b36b68d48e3cd513f53068
maram <mameeal@gmail.com>
2019-04-20 21:36:39 +0400

    Make basice settings of my project

--- blogger/blogger/settings.py
+++ blogger/blogger/settings.py
@@ -47,6 +47,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'blog',
+    'user',
 ]
 
 ROOT_URLCONF = 'blogger.urls'
